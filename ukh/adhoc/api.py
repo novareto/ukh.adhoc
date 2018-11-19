28a4f20e2646db69fa791015167b05bbb5f1bb10
Christian Klinger <ck@novareto.de>
2018-11-19 14:28:16 +0100

    chagnes

--- ukh/adhoc/api.py
+++ ukh/adhoc/api.py
@@ -2,16 +2,25 @@
 # # Copyright (c) 2007-2013 NovaReto GmbH
 # # cklinger@novareto.de
 
-
+import json
 import grok
+
+from zope import component
 from ukh.adhoc.interfaces import IUKHAdHocApp
+from zope.pluggableauth.interfaces import IAuthenticatorPlugin
 
 
 class AdHocService(grok.JSON):
     grok.context(IUKHAdHocApp)
 
+    @property
+    def users(self):
+        auth_util = component.getUtility(IAuthenticatorPlugin, name="users")
+        return auth_util.user_folder
+
     def add(self):
-        return {}
+        body = json.loads(self.body)
+        return body
 
     def update(self):
         return {}
